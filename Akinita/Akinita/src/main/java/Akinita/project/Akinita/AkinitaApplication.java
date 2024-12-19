514bcb82d8e690930e5d1f3d4fb8679c0a1eda63
it2022055 <it2022055@hua.gr>
2024-12-19 17:52:42 +0200

    epiteloius lysi

--- Akinita/Akinita/src/main/java/Akinita/project/Akinita/AkinitaApplication.java
+++ Akinita/Akinita/src/main/java/Akinita/project/Akinita/AkinitaApplication.java
@@ -1,4 +1,4 @@
-package Akinita.project.hua.Akinita;
+package Akinita.project.Akinita;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- Akinita/Akinita/src/test/java/Akinita/project/Akinita/AkinitaApplicationTests.java
+++ Akinita/Akinita/src/test/java/Akinita/project/Akinita/AkinitaApplicationTests.java
@@ -1,4 +1,4 @@
-package Akinita.project.hua.Akinita;
+package Akinita.project.Akinita;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
