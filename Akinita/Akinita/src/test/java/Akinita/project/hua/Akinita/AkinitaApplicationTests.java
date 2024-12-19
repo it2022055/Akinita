b7c9f7b25dff1d45ecbe4e26d63de2b5125db791
it2022055 <it2022055@hua.gr>
2024-12-19 16:47:24 +0200

    Εισαγωγή του project στο repo

---
+++ Akinita/Akinita/src/test/java/Akinita/project/hua/Akinita/AkinitaApplicationTests.java
@@ -0,0 +1,13 @@
+package Akinita.project.hua.Akinita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkinitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
